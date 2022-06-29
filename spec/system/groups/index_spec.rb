b66e6373d6c57756971fa0fa022c5bc90ac89b19
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-29 16:01:22 -0600

    Add groups tests

---
+++ spec/system/groups/index_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Groups(Categories)#index', type: :system do
+  subject do
+    test_user = User.create(name: 'Tom', email: 'tom@example.com', password: 'password')
+    @group = Group.create(author: test_user, name: 'food', icon: 'https://uxwing.com/wp-content/themes/uxwing/download/20-food-and-drinks/meal-food.png')
+    Group.create(author: test_user, name: 'food2', icon: 'https://')
+    @group.budget_transactions.create(author: test_user, name: 'amount test 1', amount: 1)
+    @group.budget_transactions.create(author: test_user, name: 'amount test 2', amount: 2.5)
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'tom@example.com'
+    fill_in 'Password', with: 'password'
+
+    click_button 'Log In'
+  end
+
+  it 'should visit splash page when user is not logged' do
+    visit groups_path
+    expect(page).to have_link('LOG IN')
+    expect(page).to have_link('SIGN UP')
+  end
+
+  it 'should have content category name, image, total amount for all categories' do
+    subject
+    visit groups_path
+    expect(page).to have_content('food')
+    expect(page).to have_selector('img[src="https://uxwing.com/wp-content/themes/uxwing/download/20-food-and-drinks/meal-food.png"]')
+    expect(page).to have_content('Total amount: $3.50')
+    expect(page).to have_content('food2')
+    expect(page).to have_content('Total amount: $0.00')
+  end
+
+  it 'should access the budget transactions path for this cataegory when clicking the card' do
+    subject
+    visit groups_path
+    click_link 'food'
+    expect(page).to have_current_path(group_budget_transactions_path(@group.id))
+  end
+
+  it 'should access the new category form when clicking add new category' do
+    subject
+    visit groups_path
+    click_link 'ADD NEW CATEGORY'
+    expect(page).to have_current_path(new_group_path)
+  end
+end
